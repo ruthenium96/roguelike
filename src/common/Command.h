a6b6c3933abfa44377b2ab25337ac2ae7a15cb20
Eugen Kadilenko <emkad@yandex.ru>
2022-04-14 21:57:11 +0700

    Add common and Command.

---
+++ src/common/Command.h
@@ -0,0 +1,11 @@
+#ifndef ARCH_ROGUELIKE_COMMAND_H
+#define ARCH_ROGUELIKE_COMMAND_H
+
+// This class helps modules communicate each other.
+namespace common {
+class Command {
+
+};
+}
+
+#endif //ARCH_ROGUELIKE_COMMAND_H
