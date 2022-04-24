188aa931a79093bb5f62a8b3cd0e7116703f95c9
Eugen Kadilenko <emkad@yandex.ru>
2022-04-24 18:59:04 +0700

    Add unit tests and CI.

---
+++ tests/unit/engine/state_tests.cpp
@@ -0,0 +1,121 @@
+#include "gtest/gtest.h"
+
+#include "../../../src/world/generator/AbstractGenerator.h"
+#include "../../../src/world/state/State.h"
+#include "../../../src/world/state/action/concrete/PlayerMove.h"
+
+namespace world::generator {
+class GeneratorForTests : public AbstractGenerator {
+public:
+    void addPlayerPublic(common::Coordinate coordinate, std::vector<ObjectAndActions>& answer, uint64_t& generated_identities_) {
+        addPlayer(coordinate, answer, generated_identities_);
+    };
+    void addFloorPublic(common::Coordinate coordinate, std::vector<ObjectAndActions>& answer, uint64_t& generated_identities_) {
+        addFloor(coordinate, answer, generated_identities_);
+    };
+    void addWallPublic(common::Coordinate coordinate, std::vector<ObjectAndActions>& answer, uint64_t& generated_identities_) {
+        addWall(coordinate, answer, generated_identities_);
+    };
+    void addArtefactPublic(common::Coordinate coordinate, std::vector<ObjectAndActions>& answer, uint64_t& generated_identities_) {
+        addArtefact(coordinate, answer, generated_identities_);
+    };
+    std::vector<ObjectAndActions> generateObjects(common::Coordinate coordinate,
+                                                  const state::object::Observer &observer) override {
+        return {};
+    }
+};
+}
+
+TEST(state_tests, canWalkOntoFloor) {
+    world::state::State state;
+    world::generator::GeneratorForTests generator;
+
+    std::vector<world::generator::ObjectAndActions> answer;
+    uint64_t generated_identities;
+    // .......
+    // .......
+    // .......
+    // ...P...
+    // .......
+    // .......
+    // .......
+    generator.addPlayerPublic({0, 0}, answer, generated_identities);
+    for (int x = -2; x <= 2; ++x) {
+        for (int y = -2; y <= 2; ++y) {
+            generator.addFloorPublic({x, y}, answer, generated_identities);
+        }
+    }
+
+    for (const auto& objectAndAction : answer) {
+        state.getObjectObserver().addObject(objectAndAction.object);
+        for (const auto& action : objectAndAction.actions) {
+            state.getActionObserver().addAction(action);
+        }
+    }
+    common::Coordinate expectedCoordinate = {0, 0};
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    auto moveActionRight = std::make_shared<world::state::action::PlayerMove>(1, 0);
+    auto moveActionLeft = std::make_shared<world::state::action::PlayerMove>(-1, 0);
+    auto moveActionUp = std::make_shared<world::state::action::PlayerMove>(0, -1);
+    auto moveActionDown = std::make_shared<world::state::action::PlayerMove>(0, 1);
+
+    state.applyAction(moveActionRight);
+    expectedCoordinate.x += 1;
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    state.applyAction(moveActionUp);
+    expectedCoordinate.y -= 1;
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    state.applyAction(moveActionUp);
+    expectedCoordinate.y -= 1;
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    state.applyAction(moveActionLeft);
+    expectedCoordinate.x -= 1;
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    state.applyAction(moveActionDown);
+    expectedCoordinate.y += 1;
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+}
+
+TEST(state_tests, cannotWalkIntoWall) {
+    world::state::State state;
+    world::generator::GeneratorForTests generator;
+
+    std::vector<world::generator::ObjectAndActions> answer;
+    uint64_t generated_identities;
+    //  #
+    // #P#
+    //  #
+    generator.addPlayerPublic({0, 0}, answer, generated_identities);
+    generator.addFloorPublic({0, 0}, answer, generated_identities);
+    generator.addWallPublic({0, 1}, answer, generated_identities);
+    generator.addWallPublic({0, -1}, answer, generated_identities);
+    generator.addWallPublic({1, 0}, answer, generated_identities);
+    generator.addWallPublic({-1, 0}, answer, generated_identities);
+
+    for (const auto& objectAndAction : answer) {
+        state.getObjectObserver().addObject(objectAndAction.object);
+        for (const auto& action : objectAndAction.actions) {
+            state.getActionObserver().addAction(action);
+        }
+    }
+    common::Coordinate expectedCoordinate = {0, 0};
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+
+    auto moveAction = std::make_shared<world::state::action::PlayerMove>(1, 0);
+    state.applyAction(moveAction);
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+    moveAction = std::make_shared<world::state::action::PlayerMove>(-1, 0);
+    state.applyAction(moveAction);
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+    moveAction = std::make_shared<world::state::action::PlayerMove>(0, 1);
+    state.applyAction(moveAction);
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+    moveAction = std::make_shared<world::state::action::PlayerMove>(0, -1);
+    state.applyAction(moveAction);
+    ASSERT_EQ(state.getObjectObserver().getPlayer()->getCoordinate(), expectedCoordinate);
+}
