b02a9a00255472861f4b9ad0d494122017be8cd3
Eugen Kadilenko <emkad@yandex.ru>
2022-05-14 16:30:21 +0700

    Add Mold tests, turn off Poison.

--- src/world/generator/AbstractGenerator.cpp
+++ src/world/generator/AbstractGenerator.cpp
@@ -26,10 +26,10 @@ void AbstractGenerator::addPlayer(common::Coordinate coordinate,
     player.object = std::make_shared<state::object::Player>(playerIdentity);
     player.object->getCoordinate() = coordinate;
     // add Player actions
-    auto actionIdentity = state::IdentityGenerator::getNewIdentity();
-    auto poisonAction = std::make_shared<state::action::Poison>(actionIdentity, -1, 100000);
-    poisonAction->setCorrespondingObjectIdentity(playerIdentity);
-    player.actions.push_back(poisonAction);
+//    auto actionIdentity = state::IdentityGenerator::getNewIdentity();
+//    auto poisonAction = std::make_shared<state::action::Poison>(actionIdentity, -1, 100000);
+//    poisonAction->setCorrespondingObjectIdentity(playerIdentity);
+//    player.actions.push_back(poisonAction);
     // ...
     answer.push_back(player);
 }
